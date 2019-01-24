c834980f770c17d043e9655fea2c583783b1216d
marcelo-vp <marcelos.mail@gmail.com>
2019-01-23 22:15:30 -0200

    Updating webpack to use custom html entry

--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,23 @@
-import { Counter } from './design-patterns/RevealingModule';
+import { basicSingleton, SingletonTester } from './design-patterns/Singleton';
 
-/**
- * Instance example
-*/
-Counter.start();
-Counter.increment();
+var singletonOne = basicSingleton.getInstance();
+var singletonTwo = basicSingleton.getInstance();
+var privateNumberOne = singletonOne.getPrivateNumber();
+var privateNumberTwo = singletonTwo.getPrivateNumber();
 
-console.log( Counter.getCount() );
+console.log( privateNumberOne === privateNumberTwo );
+// Returns true, because both 'singletonOne' and
+// 'singletonTwo' hold the same Singleton instance
+
+var singletonTestOne = SingletonTester.getInstance({
+    pointX: 4,
+    pointY: 25
+});
+var singletonTestTwo = SingletonTester.getInstance();
+
+console.log( singletonTestOne.name );
+console.log( singletonTestOne.name === singletonTestTwo.name );
+console.log( singletonTestOne.pointX );
+console.log( singletonTestOne.pointX === singletonTestTwo.pointX );
+console.log( singletonTestOne.pointY );
+console.log( singletonTestOne.pointY === singletonTestTwo.pointY );
